1c688e5f94c505020476deb3850166ef83c158a4
John Thomas Shaw Valanidas <John.Valanidas@gmail.com>
2017-04-18 20:05:30 -0400

    Basic game complete, still needs styling

--- game.rb
+++ game.rb
@@ -2,14 +2,13 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 enable :sessions
-@@input = []
-@@feedback = []
-@@html = ""
-@@started = false;
 @@colors = ["red","blue","green","orange","purple","pink"]
 @@code = ["red","blue","red","red"]
-@@turns = 0;
 
+
+def random_code
+  return [@@colors[rand(6)],@@colors[rand(6)],@@colors[rand(6)],@@colors[rand(6)]]
+end
 
 def build_html
   row = "<li class=\"turn-row\">" + "\n"
@@ -17,7 +16,24 @@ def build_html
   build_html_feedback(row)
   row << "</li>" + "\n"
   @@html = row << @@html
+  game_over?
+end
 
+def reset
+  @@turns = 0
+  @@input = []
+  @@feedback = []
+  @@html = ""
+  @@game_result = ""
+  @@code = random_code
+end
+
+def run params
+  reset if @@turns == 12
+  @@turns += 1
+  interpert_input(params)
+  build_feedback
+  build_html
 end
 
 def build_html_input row
@@ -36,11 +52,36 @@ def build_html_feedback row
   row << "</ul>" + "\n"
 end
 
+def build_html_win
+  @@game_result << "<h1 class=\"game-result\">You've Won!</h1>" + "\n"
+end
+
+def build_html_loss
+  @@game_result << "<h1 class=\"game-result\">You've Lost!</h1>" + "\n"
+  @@game_result << "<ul class=\"secret-code\">" + "\n"
+  @@code.each do |color|
+    @@game_result << "<li><div class=\"circle #{color}\"></div></li>" + "\n"
+  end
+  @@game_result << "</ul"> + "\n"
+end
+
 def interpert_input params
   @@input.push([params['selection_1'].to_s,
                params['selection_2'].to_s,
                params['selection_3'].to_s,
                params['selection_4'].to_s])
+end
+
+def game_over?
+  if @@turns >= 1
+    if @@input[-1] == @@code
+      build_html_win
+      return
+    end
+    if @@turns == 12
+      build_html_loss
+    end
+  end
 end
 
 def build_feedback
@@ -73,14 +114,17 @@ def build_feedback
   @@feedback.push(feedback)
 end
 
+
+
+# actual server
+reset
 get '/' do
-  erb :index, :locals => {:game_board => @@html}
+  erb :index, :locals => {:game_board => @@html,
+                          :game_result => @@game_result}
 end
 
 post '/' do
-  @@turns += 1
-  interpert_input(params)
-  build_feedback
-  build_html
-  erb :index, :locals => {:game_board => @@html}
+  run params
+  erb :index, :locals => {:game_board => @@html,
+                          :game_result => @@game_result}
 end
